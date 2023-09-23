011dea0afaa39002263a6621977587df2c3e65ca
Elva0508 <idawg071010wk@gmail.com>
2023-09-23 08:11:37 +0800

    fix: variable font-size scss styles

--- client/hooks/useRWD.js
+++ client/hooks/useRWD.js
@@ -1,29 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 const useRWD = () => {
   const [device, setDevice] = useState("PC");
-  useEffect(() => {
-    // 定义一个函数来初始化设备类型
-    function initializeDevice() {
-      const width = window.innerWidth;
-      width <= 375
-        ? setDevice("mobile")
-        : width > 375 && width <= 768
-        ? setDevice("tablet")
-        : setDevice("PC");
-    }
 
-    // 添加窗口大小变化的事件监听器，并在组件卸载时移除它
-    window.addEventListener("resize", initializeDevice);
-
-    // 初始设备类型
-    initializeDevice();
-
+  useLayoutEffect(() => {
+    window.addEventListener("resize", handleRWD);
+    handleRWD();
     return () => {
-      window.removeEventListener("resize", initializeDevice);
+      window.removeEventListener("resize", handleRWD);
     };
-  }, []);
+  }, [device]);
 
+  function handleRWD() {
+    const width = window.innerWidth;
+    width <= 375
+      ? setDevice("mobile")
+      : width > 375 && width <= 768
+      ? setDevice("tablet")
+      : setDevice("PC");
+  }
   return device;
 };
 
