29919853c470c3313e6dab9a29195b6a2b983a36
nonolin1128 <taiyijob@gmail.com>
2023-09-24 14:21:09 +0800

    文章列表頁

--- client/pages/article-list.js
+++ client/pages/article-list.js
@@ -1,18 +1,36 @@
 import React from "react";
-import BreadCrumb from '@/components/article/bread-crumb';
-import ArticleCatogory from '@/components/article/article-catogory';
+import BreadCrumb from "@/components/article/bread-crumb";
+import ArticleCatogory from "@/components/article/article-catogory";
+import ArticleListCard from "@/components/article/article-list-card";
 
 export default function ArticleList() {
   return (
     <>
       <h1>ArticleList</h1>
       <nav>navbar</nav>
-      <div>麵包屑</div>
-      <BreadCrumb/>
-      <div>類別按鈕</div>
-      <ArticleCatogory/>
-      <div>四列文章列表，一列三個</div>
-      <div>分頁</div>
+      <div className="container">
+        <div className="mt-5">
+          <BreadCrumb />
+        </div>
+
+        <div className="d-flex justify-content-center mb-5 ">
+          <ArticleCatogory />
+        </div>
+
+        <div className="mb-5">
+          <ArticleListCard />
+        </div>
+        <div className="mb-5">
+          <ArticleListCard />
+        </div>
+        <div className="mb-5">
+          <ArticleListCard />
+        </div>
+        <div className="mb-5">
+          <ArticleListCard />
+        </div>
+        <div>分頁</div>
+      </div>
       <footer>footer</footer>
     </>
   );
