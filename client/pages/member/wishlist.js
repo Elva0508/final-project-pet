2c59f57971c91101fdacadf9b97f5891587a8918
Carriewu0926 <carrie.x.wu@gmail.com>
2023-09-26 11:17:22 +0800

    wishlist連接資料庫
    wishlist連接資料庫

--- client/pages/member/wishlist.js
+++ client/pages/member/wishlist.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import ListM from '@/components/member/list-m'
 import ListD from '@/components/member/list-d'
 import ListUserM from '@/components/member/list-user-m'
 import { FaList } from 'react-icons/fa'
 import { RiDeleteBin5Line } from 'react-icons/ri'
+import axios from "axios"
 
 
 export default function Wishlist() {
+  const [wishlist,setWishlist]=useState([])
+
+
+  const getWishlist = async() => {
+      await axios.get("http://localhost:3005/member/wishlist")
+        .then((response) => {
+          const data = response.data.result;
+          console.log(data);
+          setWishlist(data)     
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+      });
+    }
+      useEffect(() => {
+        getWishlist()
+        }, [])
+
+
   return (
     <>
     <div className='container'>
@@ -19,30 +39,40 @@ export default function Wishlist() {
               <div className=' col-12 col-sm-8 purchast p-3'>
                 <div>
                     <h5 className='size-5'><FaList/>追蹤清單</h5>
-                    <div className='col-12 d-flex border-bottom py-2'>
-                        <div className='col-3 col-sm-3'>
-                         <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_1(12).jpg'></img>   
-                        </div>
-                            
-                        <div className='col-5 col-sm-6'>
-                            <p className='size-5'>巨型開放式貓砂盆 (多色)</p>
-                            <p className='size-6 type'>貓砂盆</p>
-                            <p className='size-5 price'>NT$690</p>
-                        </div>        
+                    
+                    {wishlist.map((v,i)=>{
+                      return(
+                        <>
+                        <div className='col-12 d-flex border-bottom py-2'>
+                          <div className='col-3 col-sm-3'key={v.collection_id}>
+                          <img src={v.image}></img>   
+                          </div>
+                              
+                          <div className='col-5 col-sm-6'>
+                              <p className='size-5'>{v.product_name}</p>
+                              <p className='size-6 type'>{v.type}</p>
+                              <p className='size-5 price'>NT${v.price}</p>
+                          </div>  
        
                         <div className='col-4 col-sm-3 align-self-center d-sm-block d-none '>
                             <button className="btn btn-confirm size-6 me-4">加入購物車</button>
-                            <RiDeleteBin5Line />
+                            <button className='delete'><RiDeleteBin5Line /></button>
                         </div>
 
                        <div className='col-4 d-sm-none d-block d-flex flex-column justify-content-between'>
-                          <div className='d-flex justify-content-end'><RiDeleteBin5Line /></div>
+                          <div className='d-flex justify-content-center'>
+                            <button className='delete'><RiDeleteBin5Line /></button>
+                          </div>
                           
                           <button className="btn btn-confirm m-2 size-6 ">加入購物車</button>
                           
                         </div>
 
                     </div>
+                        </>
+                      )
+                    })}
+
                 </div>
               </div>
 
