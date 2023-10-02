7982d501fd555f9fa5718fbca9d0916af0aa6574
Carriewu0926 <carrie.x.wu@gmail.com>
2023-10-02 16:14:56 +0800

    連接資料庫
    連接資料庫

--- client/components/member/order-status-one.js
+++ client/components/member/order-status-one.js
@@ -1,43 +1,54 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function OrderStatusOne() {
+
+    const [order, setOrder] = useState([])
+
+    const getOrder = async() => {
+        await axios.get("http://localhost:3005/api/member-order")
+          .then((response) => {
+            const data = response.data.result;
+            console.log(data);
+            setOrder(data)
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+        });
+      }
+  
+    useEffect(() => {
+        getOrder()
+      }, [])
     return (
         <>
-            <p className='date my-3 size-7'>2023-08-26 訂單編號 : 134868</p>
-            <div className='d-flex justify-content-between border-bottom pb-3'>
-                <div className='d-flex flex-column justify-content-around '>
-                    <p className='size-7'>狀態 : 尚未付款</p>
-                    <p className='size-7'>付款方式 : 貨到付款</p>
-                    <p className='size-7'>寄送方式 : 宅配</p>
-                    <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-none d-block'></img>
-                    <p className='size-6 price'>訂單金額 : NT$380</p>
-                </div>
-                <div className='d-flex align-self-end'>
-                    <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-block d-none'></img>
 
-                    <div className='d-flex align-self-center'>
-                        <button className="btn btn-outline-confirm m-2 size-6">查看明細</button>
-                    </div>
-                </div>
-            </div>
 
-            <p className='date my-3 size-7'>2023-08-26 訂單編號 : 134868</p>
-            <div className='d-flex justify-content-between border-bottom pb-3'>
-                <div className='d-flex flex-column justify-content-around '>
-                    <p className='size-7'>狀態 : 尚未付款</p>
-                    <p className='size-7'>付款方式 : 貨到付款</p>
-                    <p className='size-7'>寄送方式 : 宅配</p>
-                    <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-none d-block'></img>
-                    <p className='size-6 price'>訂單金額 : NT$380</p>
-                </div>
-                <div className='d-flex align-self-end'>
-                    <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-block d-none'></img>
+        {order.map((v,i)=>{
+                    return(
+                      <div key={i}>
+                        <p className='date my-3 size-7'>{v.created_at} 訂單編號 :{v.oid}</p>
+                        <div className='d-flex justify-content-between border-bottom'>
+                          <div>
+                            <p className='size-7'>狀態 : {v.status_name}</p>
+                            <p className='size-7'>付款方式 : {v.payment}</p>
+                            <p className='size-7'>寄送方式 : {v.shipment}</p>
+                            <img src={v.image} className='d-sm-none d-block' alt='產品圖片'></img>
+                            <p className='size-6 price'>訂單金額 : NT${v.total_amount}</p>
+                          </div>
+                          <div className='d-flex align-self-end'>
+                            <img src={v.image} className='d-sm-block d-none' alt='產品圖片'></img>
+                            <div className='d-flex align-self-center'>
+                              <button className="btn btn-outline-confirm m-2 size-6">查看明細</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      
+                    )
+                  })}
 
-                    <div className='d-flex align-self-center'>
-                        <button className="btn btn-outline-confirm m-2 size-6">查看明細</button>
-                    </div>
-                </div>
-            </div>
         </>
     )
 }
--- client/pages/member/order.js
+++ client/pages/member/order.js
@@ -19,23 +19,7 @@ export default function Order() {
     
   };
 
-  const [order, setOrder] = useState([])
 
-  const getOrder = async() => {
-      await axios.get("http://localhost:3005/api/member-order")
-        .then((response) => {
-          const data = response.data.result;
-          console.log(data);
-          setOrder(data)
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-      });
-    }
-
-  useEffect(() => {
-      getOrder()
-    }, [])
 
   return (
     <>
@@ -93,31 +77,6 @@ export default function Order() {
                   {currentScreen === "4" && (<OrderStatusFour />)}
                 </div>
 
-                {order.map((v,i)=>{
-                    return(
-                      <div key={i}>
-                        <p className='date my-3 size-7'>2023-08-26 訂單編號 : 134868</p>
-                        <div className='d-flex justify-content-between border-bottom'>
-                          <div>
-                            <p className='size-7'>狀態 : {v.status_id}</p>
-                            <p className='size-7'>付款方式 : 貨到付款</p>
-                            <p className='size-7'>寄送方式 : 宅配</p>
-                            <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-none d-block' alt='产品图片'></img>
-                            <p className='size-6 price'>訂單金額 : NT${v.total_amount}</p>
-                          </div>
-                          <div className='d-flex align-self-end'>
-                            <img src='https://cdn-front.mao-select.com.tw//upload_files/fonlego-rwd/prodpic/D_A1MKD200101(1).jpg' className='d-sm-block d-none' alt='产品图片'></img>
-                            <div className='d-flex align-self-center'>
-                              <button className="btn btn-outline-confirm m-2 size-6">查看明細</button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      
-                    )
-                  })}
-
-                { screenName==="1" && <orderStatusOne />}
               </div>
             </div>
           </div>
