f367932f37314e31303dad3128fae564d5a38170
nonolin1128 <taiyijob@gmail.com>
2023-10-02 16:27:27 +0800

    文章列表頁資料庫連結完成

--- client/components/article/article-list-card.js
+++ client/components/article/article-list-card.js
@@ -1,25 +1,35 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import axios from "axios";
+import { useRouter } from "next/router";
 
-export default function ArticleListCard({ category }) {
+export default function ArticleListCard() {
+  const router = useRouter();
   const [articleListCard, setArticleListCard] = useState([]);
 
-  const getArticleListCard = async () => {
-    await axios
-      .get(`http://localhost:3005/article/${category}`)
-      .then((response) => {
-        const data = response.data.result;
-        console.log(data);
-        setArticleListCard(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+  // 向伺服器要求資料，設定到狀態中
+  const getArticleListCard = async (article_category_id) => {
+    const res = await fetch(
+      "http://localhost:3005/article/" + article_category_id
+    );
+
+    const data = await res.json();
+
+    console.log(data);
+    // 設定到狀態中 -> 會觸發重新渲染(re-render)
+    if (Array.isArray(data)) setArticleListCard(data);
   };
+
+  // didMount 初次渲染"後", 向伺服器要求資料，設定到狀態中
   useEffect(() => {
-    getArticleListCard();
-  }, [category]);
+    if (router.isReady) {
+      // 確保能得到router.query有值
+      const { article_category_id } = router.query;
+      console.log(article_category_id);
+      // 有article_category_id後，向伺服器要求資料，設定到狀態中
+      getArticleListCard(article_category_id);
+    }
+    // eslint-disable-next-line
+  }, [router.isReady]);
 
   return (
     <>
@@ -29,7 +39,9 @@ export default function ArticleListCard({ category }) {
             return (
               <>
                 <div className="col">
-                  <Link href={`/article/${v.article_id}`}>
+                  <Link
+                    href={`/article/${v.article_category_id}/${v.article_id}`}
+                  >
                     <div className="card h-100" key={v.article_id}>
                       <img src={v.img} alt="..." />
                       <div className="card-body">
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const mysql=require("mysql2");
-const connection=require("./db");
-const express=require("express");
-const app=express();
-const cors = require('cors');
-const bodyParser=require('body-parser')
+const mysql = require("mysql2");
+const connection = require("./db");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const memberRouter = require("./routes/member-route");
 const workRouter = require("./routes/work-route");
@@ -13,7 +13,7 @@ const cartRouter = require("./routes/cart-route");
 
 app.use(cors());
 app.use(bodyParser.json()); // 解析 JSON 请求体
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,20 +23,27 @@ app.use("/api/member-order", memberOrderRouter);
 app.use("/api/member-wishlist", memberWishlistRouter);
 app.use("/api/product/cart", cartRouter);
 
-
-
-app.get("/article", (req, res) => {
+app.get("/article/:cid", (req, res) => {
+  let cid = req.params.cid;
   connection.execute(
     `SELECT *
     FROM article
     JOIN article_category ON article.article_category_id = article_category.article_category_id
-    JOIN article_images ON article.article_id = article_images.article_id;`,
+    JOIN article_images ON article.article_id = article_images.article_id
+    WHERE article.article_category_id = ?`,
+    [cid], // 使用文章的 article_id 篩選特定文章
     (error, result) => {
       if (error) {
         console.error(error);
         res.status(500).json({ error: "資料庫查詢失敗" });
       } else {
-        res.json({ result });
+        if (result.length > 0) {
+          // 如果找到了文章，回傳
+          res.json(result);
+        } else {
+          // 如果未找到文章，回覆錯誤
+          res.status(404).json({ error: "文章未找到" });
+        }
       }
     }
   );
@@ -86,6 +93,3 @@ app.get("/article/:cid/:id", (req, res) => {
 app.listen(3005, () => {
   console.log("server is running");
 });
-app.listen(3005,()=>{
-    console.log("server is running");
-})
