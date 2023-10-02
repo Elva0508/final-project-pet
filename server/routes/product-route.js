191b1c68e939741031fa3adf2b8da5e002e5c0ee
tsaichiachi <spi890011@gmail.com>
2023-10-03 00:26:33 +0800

    前後端連線

--- server/routes/product-route.js
+++ server/routes/product-route.js
@@ -4,17 +4,32 @@ const connection = require("../db");
 router.get("/", (req, res) => {
     connection.execute(
         `SELECT
-        p.*
+            p.product_id,
+            p.product_name,
+            p.vendor,
+            p.category_id,
+            p.subcategory_id,
+            p.description,
+            p.price,
+            p.specialoffer,
+            p.quantity,
+            p.sales,
+            p.created_at,
+            p.images_one,
+            p.images_two,
+            p.images_three,
+            p.images_four
         FROM
-        products AS p
+            products AS p
         JOIN
-        subcategory AS s
+            subcategory AS s
         ON
-        p.subcategory_id = s.subcategory_id`,
+            p.subcategory_id = s.subcategory_id`,
         (error, result) => {
             res.json({ result });
         }
     );
 });
+
 
 module.exports = router;
