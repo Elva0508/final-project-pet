4632234ff4aae0c462c3a63923b2d677c50ccae2
Carriewu0926 <carrie.x.wu@gmail.com>
2023-10-04 01:58:53 +0800

    完成收藏加入購物車
    完成收藏加入購物車

--- server/routes/member-wishlist-route.js
+++ server/routes/member-wishlist-route.js
@@ -4,17 +4,28 @@ const connection=require("../db");
 //收藏清單
 router.get("/", (req, res) => {
     connection.execute(
-      `SELECT pc.*, p.product_name AS product_name, p.product_type AS type,p.specialoffer AS price, i.images_one AS image 
-          FROM product_collections AS pc 
-          JOIN users AS u ON pc.user_id=u.user_id 
-          JOIN products AS p ON pc.product_id = p.product_id 
-          JOIN images_product AS i ON p.product_group_id = i.id 
-          WHERE pc.user_id = 1;`,
+      `SELECT pc.*, p.product_name AS product_name,pt.type_name AS type_name,p.images_one AS images,p.specialoffer AS price
+      FROM product_collections AS pc 
+      JOIN users AS u ON pc.user_id=u.user_id 
+      JOIN products AS p ON pc.product_id = p.product_id 
+      JOIN product_type AS pt ON pc.product_id=pt.product_id
+      WHERE pc.user_id = 1
+      GROUP BY pc.product_id;`,
       (error, result) => {
         res.json({ result });
       }
     );
   });
+
+//收藏商品規格
+router.get("/type", (req, res) => {
+  connection.execute(
+    `SELECT pt.type_name AS type_name,pt.type_id AS type_id,pc.product_id FROM product_collections AS pc JOIN users AS u ON pc.user_id=u.user_id JOIN product_type AS pt ON pc.product_id=pt.product_id WHERE pc.user_id = 1`,
+    (error, result) => {
+      res.json({ result });
+    }
+  );
+});
 //刪除收藏清單
 router.delete("/:id",(req,res)=>{
     const deleteId=req.params.id
@@ -39,11 +50,11 @@ router.get("/cart",(req,res)=>{
     )
 })
   //用來新增購物車裡沒有的商品
-  router.put("/cart/:id",(req,res)=>{
-    const addId=req.params.id   
+  router.put("/cart",(req,res)=>{
+    const {id ,type}=req.body  
     connection.execute(
-        `INSERT INTO cart(user_id, product_id, quantity) VALUES (1,?,1);`,
-        [addId]
+        `INSERT INTO cart(user_id, product_id,  product_type_id,quantity) VALUES (1,?,?,1);`,
+        [id,type]
         ,(error,result)=>{
             res.json({result})
         }    
@@ -52,10 +63,10 @@ router.get("/cart",(req,res)=>{
 //用來修改購物車裡已經有的商品數量
 router.put("/cartplus",(req,res)=>{
     console.log(req);
-    const {id ,newQuantity}=req.body
+    const {id ,newQuantity,type}=req.body
     connection.execute(
-        `UPDATE cart SET quantity=? WHERE user_id=1 AND product_id=?`,
-        [ newQuantity,id]
+        `UPDATE cart SET quantity=? WHERE user_id=1 AND product_id=? AND product_type_id=?`,
+        [ newQuantity,id,type]
         ,(error,result)=>{
             res.json({result})
         }    
