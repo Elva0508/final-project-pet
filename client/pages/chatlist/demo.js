36c00ba89ad3f3d03b238f535f710c71267cd23f
nonolin1128 <taiyijob@gmail.com>
2023-10-13 16:22:04 +0800

    聊天室功能優化

--- client/pages/chatlist/demo.js
+++ client/pages/chatlist/demo.js
@@ -4,8 +4,11 @@ import axios from "axios";
 
 export default function ChatDemo() {
   const [message, setMessage] = useState(""); // 儲存返回後的消息
+  // const [chatContent, setChatContent] = useState([]); //內容設置的狀態
   const [isLoading, setIsLoading] = useState(false);
   const [userId, setUserId] = useState(null); // 用於儲存解析後的userID
+  const [chatlistId, setChatlistId] = useState(null); // 用於儲存解析後的chatlistId
+  const [msgInputValue, setMsgInputValue] = useState(""); // 輸入框的值
 
   // 利用token拿到當前登入的userID
   useEffect(() => {
@@ -22,6 +25,7 @@ export default function ChatDemo() {
     }
   }, []);
 
+  // 發出消息找有沒有chatlist_id 沒有就新增
   const handleButtonClick = async () => {
     setIsLoading(true);
 
@@ -44,14 +48,30 @@ export default function ChatDemo() {
           // 請求成功
           setMessage("請求成功");
           const chatUrl = response.data.chatUrl;
+          const ChatlistId = response.data.chatlistId;
           console.log("chatUrl" + chatUrl);
+          console.log("ChatlistId" + ChatlistId);
+
+          //把ChatlistId存到狀態裡後面送消息時使用
+          setChatlistId(ChatlistId);
+
+          // 调用handleSendClick
+          handleSendClick(ChatlistId);
           // 在這裡導向到 chatUrl
           window.location.href = chatUrl;
         } else if (response.status === 200) {
           // 消息已存在
           setMessage("消息已存在");
           const chatUrl = response.data.chatUrl;
+          const ChatlistId = response.data.chatlistId;
           console.log("已存在chatUrl" + chatUrl);
+          console.log("ChatlistId" + ChatlistId);
+
+          //把ChatlistId存到狀態裡後面送消息時使用
+          setChatlistId(ChatlistId);
+
+          // 调用handleSendClick
+          handleSendClick(ChatlistId);
           // 在這裡導向到 chatUrl
           window.location.href = chatUrl;
         } else {
@@ -67,6 +87,39 @@ export default function ChatDemo() {
     }
   };
 
+  // 處理訊息送出事件
+  const handleSendClick = async () => {
+    // 先檢查消息是否為空
+    if (!msgInputValue) {
+      return;
+    }
+    try {
+      // 發送消息到後端
+      const response = await fetch(
+        "http://localhost:3005/api/chatroom/sendchat",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chatlist_id: chatlistId,
+            talk_userId: userId, // 使用前端頁面登入的 userId
+            chat_content: msgInputValue,
+          }),
+        }
+      );
+      if (response.ok) {
+        // 清空輸入框的值
+        setMsgInputValue("");
+      } else {
+        console.error("發送消息時出錯");
+      }
+    } catch (error) {
+      console.error("發送消息時出錯", error);
+    }
+  };
+
   return (
     <>
       <h1>ChatDemo</h1>
@@ -79,6 +132,26 @@ export default function ChatDemo() {
       </button>
       {/* 顯示返回後的消息 */}
       {message && <p>{message}</p>}
+      <div className="input-group mb-3">
+        <input
+          name="msg"
+          type="text"
+          className="form-control"
+          placeholder="請輸入訊息內容"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          value={msgInputValue}
+          onChange={(e) => setMsgInputValue(e.target.value)}
+        />
+        <button
+          className="btn-second mx-1"
+          type="button"
+          id="button-addon2"
+          onClick={handleButtonClick}
+        >
+          {isLoading ? "送出中..." : "送出"}
+        </button>
+      </div>
     </>
   );
 }
