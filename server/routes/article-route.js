adbd60b1e02793d7a3360ff8e087cc3827f9843d
nonolin1128 <taiyijob@gmail.com>
2023-10-13 14:20:29 +0800

    小貓兩三知index完成

--- server/routes/article-route.js
+++ server/routes/article-route.js
@@ -68,7 +68,7 @@ router.get("/:cid/:id", (req, res) => {
   );
 });
 
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
   connection.execute(
     `SELECT *
       FROM article
@@ -76,6 +76,29 @@ router.get("/", (req, res) => {
       JOIN article_images ON article.article_id = article_images.article_id
       ORDER BY article.published_date DESC -- 照日期排序
       LIMIT 9; -- 只要九筆資料
+    `,
+    (error, result) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({ error: "資料庫查詢失敗" });
+      } else {
+        if (result.length > 0) {
+          res.json(result);
+        } else {
+          res.status(404).json({ error: "文章未找到" });
+        }
+      }
+    }
+  );
+});
+
+router.get("/", (req, res) => {
+  connection.execute(
+    `SELECT *
+      FROM article
+      JOIN article_category ON article.article_category_id = article_category.article_category_id
+      JOIN article_images ON article.article_id = article_images.article_id
+      ORDER BY article.published_date DESC -- 照日期排序
     `,
     (error, result) => {
       if (error) {
