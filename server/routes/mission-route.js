c32580c8aa8322b829fe374b2e01f59fa82b5a4a
ru0618 <riley60530@gmail.com>
2023-10-17 18:13:03 +0800

    修復google地圖無法出現問題
    因為資料庫有刪減 抓到了不存在的資料庫

--- server/routes/mission-route.js
+++ server/routes/mission-route.js
@@ -306,13 +306,10 @@ router.get("/mission-details-map/:mission_id", (req, res) => {
   const mission_id = req.params.mission_id; // 從路由參數中獲取 mission_id
   conn.execute(
     `
-    SELECT md.*, u.*, GROUP_CONCAT(DISTINCT im.file_path ORDER BY im.image_id) AS file_paths
+    SELECT md.*
     FROM mission_detail AS md 
-    JOIN users AS u ON md.post_user_id = u.user_id 
-    JOIN image_mission AS im ON md.mission_id = im.mission_id
     WHERE md.mission_id = ?
-    GROUP BY md.mission_id;
-    `,
+    ;`,
     [mission_id],  // 使用 mission_id 進行查詢
     (err, result) => {
       if (err) {
@@ -400,7 +397,7 @@ router.get("/popular", (req, res) => {
     JOIN image_mission AS im ON min_ids.mission_id = im.mission_id AND min_ids.min_image_id = im.image_id
     GROUP BY md.mission_id
     ORDER BY user_count DESC
-    LIMIT 5    
+    LIMIT 6    
     ;`,
     (error, result) => {
       res.json({ result });
