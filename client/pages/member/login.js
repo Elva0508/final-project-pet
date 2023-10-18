3cff5fdacfffd6acc8d2baf62cdfa18c2eb05111
Melody1106 <101308244+Melody1106@users.noreply.github.com>
2023-10-18 14:01:01 +0800

    login頁面樣式修改-字體調整

--- client/pages/member/login.js
+++ client/pages/member/login.js
@@ -32,7 +32,7 @@ const router=useRouter()
         <div className="login-box d-flex align-items-center flex-column justify-content-center mx-auto ">
           <div className="login-header d-flex ">
 
-            <div className="size-5 mx-3 my-2">
+            <div className="size-4 mx-4 my-2">
               <Link
                 className={ `login-lg mx-3 ${isLogin ? "active" : ""}`}
                 href="http://localhost:3000/member/login"
@@ -41,7 +41,7 @@ const router=useRouter()
                 登入
               </Link>
             </div>
-            <div className="size-5 mx-3 my-2">
+            <div className="size-4 mx-4 my-2">
               <Link
                 className={ `login-lg mx-3 ${!isLogin ? "active" : ""}`}
                 href="http://localhost:3000/member/login"
