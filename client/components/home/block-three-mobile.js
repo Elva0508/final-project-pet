a18bbadf59dac4f2ac0bd159c81d6062cd101c15
nonolin1128 <taiyijob@gmail.com>
2023-10-18 16:21:56 +0800

    細節調整

--- client/components/home/block-three-mobile.js
+++ client/components/home/block-three-mobile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-export default function BlockThree() {
+export default function BlockThreeMobile() {
   return (
     <>
       <div className="mb-5">
